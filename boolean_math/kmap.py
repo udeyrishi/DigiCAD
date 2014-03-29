57beea059c073b251a3008fdbc0dee67614504cb
Udey Rishi <udey.rishi@gmail.com>
2014-03-28 22:41:27 -0700

    started working on bool math

---
+++ boolean_math/kmap.py
@@ -0,0 +1,9 @@
+"""
+The Karnough Map (K-map) module for the DigiCAD.
+Runs the under-the-hood Boolean algebra required by it.
+
+K-map is a specialised matrix. 
+"""
+
+class kmap:
+    def __init__(self, num_vars = 0)
