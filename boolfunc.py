72ccdf5edd383cab411227b0adece5fdbde09a7c
Udey Rishi <udey.rishi@gmail.com>
2014-04-06 15:06:38 -0600

    Added simplification. Renamed methods to more accurate names. Added a lot of functionality (see methods)

--- boolfunc.py
+++ boolfunc.py
@@ -1,13 +1,10 @@
 """
 The Boolean Function module for the DigiCAD.
 Runs the under-the-hood Boolean algebra required by it.
-
-The K-map is based on the Quine-McCluskey algorithm (instead of the traditional 
-matrix form).
 """
 
 from truth_tables import *
-import copy
+import copy, math, pdb
 
 class InvalidBooleanFunctionError(Exception):
     pass
@@ -28,12 +25,13 @@ class BF:
 
         # Parsing out the list of variables in the expression and creating the
         # truth table
-        variables, table = make_table(function)
+        variables, table, symbols = make_table(function)
         
         minterms = [i for i in table if table[i]]
         maxterms = [i for i in table if not table[i]]
 
         # Testing consistency of # of expressions
+        """
         okay = test_vals(minterms)
         if not okay: 
             raise InvalidBooleanFunctionError("Inconsistent number of expressions")
@@ -41,10 +39,11 @@ class BF:
         okay = test_vals(maxterms)
         if not okay:
             raise InvalidBooleanFunctionError("Inconsistent number of expressions")
-
+        """
+        """
         if len(variables) != len(minterms[0]):
             raise InvalidBooleanFunctionError("Inconsistent number of expressions")
-        
+        """
         # Everything is good so store the vars, TT and the expression
         self._expression = function
         self._variables = variables
@@ -113,8 +112,22 @@ class BF:
     def variables(self):
         return copy.deepcopy(self._variables)
 
+    def _varstring(self):
+            """
+            Helper method that converts the list of variables to a string that 
+            can be used in the outputs of any method that generates another 
+            BF. 
+            """
+            variables = ""
+            for i in self.variables():
+                variables += i + ", "
+            return variables[:-2]
+
     def truthtable(self):
         return copy.deepcopy(self._table)
+
+    def name(self):
+        return copy.deepcopy(self._name)
 
     def __str__(self):
         return copy.deepcopy(self._expression)
@@ -128,9 +141,9 @@ class BF:
         except AttributeError:
             raise InvalidBooleanFunctionError("Object isn't a Boolean Function!")
 
-    def sop(self):
+    def min_expand(self):
         """
-        Returns the s-o-p form of the boolean function.
+        Returns the minterm expansion of the boolean function.
         """
         rv = ""
         for num_minterm in self.minterms():
@@ -151,21 +164,20 @@ class BF:
                     term += "~" + self.variables()[i]
             rv += term
         
-        variables = ""
-        for i in self.variables():
-            variables += i + ", "
-        variables = variables[:-2]
+        if rv == "": rv = "0"
+
+        variables = self._varstring()
 
         # Since it is the same Boolean function, all the attributes except the
         # expression are same. So just copy and return. Makes processing MUCH
         # faster
         rv2 = copy.deepcopy(self)
-        rv2._expression = ("f(%s) = %s" %(variables, rv))
+        rv2._expression = ("%s_min_expand(%s) = %s" %(self.name(), variables, rv))
         return rv2        
                     
-    def pos(self):
+    def max_expand(self):
         """
-        Returns the p-o-s form of the boolean function.
+        Returns the maxterm expansion form of the boolean function.
         """
         rv = ""
         for num_maxterm in self.maxterms():
@@ -186,22 +198,30 @@ class BF:
                     factor += self.variables()[i]
             rv += factor + ")"
         
-        variables = ""
-        for i in self.variables():
-            variables += i + ", "
-        variables = variables[:-2]
+        if rv == "": rv = "1"
+        variables = self._varstring()
         
         # Since it is the same Boolean function, all the attributes except the
         # expression are same. So just copy and return. Makes processing MUCH
         # faster
         rv2 = copy.deepcopy(self)
-        rv2._expression = ("f(%s) = %s" %(variables, rv))
+        rv2._expression = ("%s_max_expand(%s) = %s" %(self.name(), variables, rv))
         return rv2
-                    
+    
+    def sub(self, values):
+        """
+        Plus in the values in the BF and returns True/False. values in a string
+        containing the values of the variables. (e.g. "1010")
+        """        
+        try:
+            return self.truthtable()[values]
+        except KeyError:
+            raise KeyError("This value does not exist in the function's truth-table")
+            
     def minimise(self):
         """
-        Finds and returns the sum-of-products form of the Boolean Function 
-        represented by the K-map.
+        Finds and returns the simplified sum-of-products form of the Boolean
+        Function.
         Uses Quine-McCluskey algorithm. 
         
         Source: http://en.wikipedia.org/wiki/Quine-McCluskey_algorithm
@@ -219,48 +239,329 @@ class BF:
         number of variables.
         """
 
-        pi = [] # Prime Implicants
-        epi = [] # Essential Prime Implicants
+        # PI contains all the different levels of PIs, such that pi[i] is the 2^i
+        # level PI.
+        
+        pis_num = self.mintermsl() # Prime implicants, PIs = numerical minterms
 
+        # Converting the minterms to a format appropriate for PIs
+        pis = {}
+
+        for i in pis_num:
+            pis[i] = [] # Create empty minterm list for all categories 
+            for j in pis_num[i]:
+                string_minterm = bin_conv(j, int(math.log(len(self.truthtable()), 2)))
+                pis[i].append(string_minterm)
+
+        pis = [pis] # Converting to list to accomodate for future additions
+
+        sim_pis = [] # Contains all the simplified prime implicants discovered so far
+
+        i = 0
+        comb_register = {}
+        while(1):
+            sim_pi_found, pis_calc, temp_register = next_pis(pis[i])
+
+            # Because simplified PIs for current PIs are generated
+            sim_pis += sim_pi_found
+
+            for merge in temp_register:
+                # For all the merged results
+
+                parents = copy.deepcopy(temp_register[merge])
+
+                for parent in parents:
+                    if parent in comb_register:
+                        # Checking if parents of merge were themselves merged earlier
+                        grandparents = comb_register[parent]
+                        # Substitute the parents with grandparents to get the minterms
+                        temp_register[merge] += grandparents
+                        temp_register[merge].remove(parent)
+
+                        # Pop off the parent to remove duplicacy
+                        #comb_register.pop(parent) 
+
+            # Record the combination results
+            comb_register.update(temp_register)
+
+            if pis_calc == {i:[] for i in self.mintermsl()}:
+                break # The most simplified version is created. So end
+            else:
+                pis.append(pis_calc)
+                i += 1
+
+        # Converting the string minterms to numeric minterms
+        for i in comb_register:
+            for j in range(len(comb_register[i])):
+                comb_register[i][j] = int(comb_register[i][j], 2)
+
+        # Generating EPIs from simplified PIs
+        epis = gen_epi(self.minterms(), sim_pis, comb_register)
+        
+        # Generating Output
+        variables = self._varstring()
+        rv = form_function(epis, self.variables())
+      
+        rv2 = copy.deepcopy(self)
+        rv2._expression = ("%s_simp(%s) = %s" %(self.name(), variables, rv))
+        return rv2
 
         
-        def next_pis(current_pi, level = 0):
-            """
-            Finds the next generation prime implicants from the current ones.
+def next_pis(current_pi):
+    """
+    Finds the next generation prime implicants from the current ones.
 
-            current_pi is a dictionary mapping [num_ones, num_dashes] to a list
-            of all the implicants in that category.
-            """
-            #if level == 0:
-                
-            next_list = []
+    current_pi is a dictionary mapping [num_ones, num_dashes] to a list
+    of all the implicants in that category.
+    If the PIs passed in are minterms, they are level 1 PIs. Function 
+    will automatically convert them to the appropriate PI format.
 
-            # Finding all the different categories of PIs
-            categories = list(current_pi)
-            categories.sort()
+    Test borrowed from Wikipedia's Quine-McCluskey algorithm page:
+    
+    >>> a = {1: ["0100", "1000"], 2: ["1001", "1010", "1100"], 3: ["1011", \
+             "1110"], 4: ["1111"]}
+    >>> sim_pis, npis, comb_register = next_pis(a)
+    >>> sim_pis == []
+    True
+    >>> npis # Order may be opposite, but correct values tested 
+    {1: ['100-', '-100', '1-00', '10-0'], 2: ['1-10', '11-0', '10-1', '101-'], 3: ['1-11', '111-'], 4: []}
 
-            # -1 because last category can't be processed
-            for i in range(len(categories) - 1):
+    >>> comb_register
+    {'-100': ['0100', '1100'],
+     '1-00': ['1000', '1100'],
+     '1-10': ['1010', '1110'],
+     '1-11': ['1011', '1111'],
+     '10-0': ['1000', '1010'],
+     '10-1': ['1001', '1011'],
+     '100-': ['1000', '1001'],
+     '101-': ['1010', '1011'],
+     '11-0': ['1100', '1110'],
+     '111-': ['1110', '1111']}
 
-                category = categories[i]
-                next_category = categories[i+1]
+    >>> sim_pis2, npis2, comb_register2 = next_pis(npis)
+    >>> sim_pis2
+    ['-100']
+    >>> npis2 # Order may be opposite, but correct values tested
+    {1: ['10--', '1--0'], 2: ['1-1-'], 3: [], 4: []}
 
-                for pi in current_pi[category]:
-                    is_epi = False
-                    # Checking for all PIs in current category
-                    for high_pi in current_pi[next_category]:
-                        # Comparing with all PIs in next category
-                        if check_combine(pi, high_pi):
-                            # If they differ by just 1 bit
-                            next_list.append(combine_pis(pi, high_pi))
-                            is_epi = False
-                            # Merge them
+    >>> comb_register2
+    {'1--0': ['10-0', '11-0'], '1-1-': ['1-10', '1-11'], '10--': ['10-0', '10-1']}
+    """
+    
+    # Contains the list of PIs formed after simplifying current stage
+    next_dict =  {}
 
-                    if is_epi:
-                        # If PI couldn't be combined, it is an EPI
-                        next_list.append(pi)
+    # Creating the categories in the next_dict
+    for i in current_pi:
+        next_dict[i] = list()
+
+    # Contains the list of PIs from current stage that are already simplified
+    sim_pis = []
+
+    # Stores all the PIs that have been merged to form next_dict
+    merged_pis = []
+
+    # Stores how the merged PI was formed (combining which previous PIs)
+    comb_register = {}
+
+    # Extracting all the categories of PIs (based on # of ones)
+    categories = list(current_pi)
+    categories.sort()
+
+    # -1 because last category can't be processed
+    for i in range(len(categories) - 1):
+
+        category = categories[i]
+        next_category = categories[i+1]
+
+        for pi in current_pi[category]:
+            # Checking for all PIs in current category
+            is_sim = True
+            
+            for high_pi in current_pi[next_category]:
+                # Comparing with all PIs in next category
+
+                combination = combine(pi, high_pi)
+                if combination:
+                    # If they differ by just 1 bit, then combination not = None
+                    # Merge them
+                    next_dict[category].append(combination)
+                    is_sim = False
+                    comb_register[combination] = [pi, high_pi]
+                    # Adding the high_pi into list merged_pis as it is technically
+                    # now merged. It is NOT the most simplified PI, even if no
+                    # higher (higher than high) mergable PI is found.
+                    merged_pis.append(high_pi)
+
+            if is_sim and pi not in merged_pis:
+                # If PI couldn't be combined, it is a simplified PI
+                sim_pis.append(pi)
+
+    # At this point, there will be duplicates of newly created PIs as different
+    # combinations can lead to the same mere.
+
+    for i in next_dict:
+        next_dict[i] = list(set(next_dict[i]))
+        
+    return sim_pis, next_dict, comb_register
+
+def combine(pi1, pi2):
+    """
+    Combines pi1 and pi2 to return the combined pi. Returns None if they can't 
+    be combined.
+
+    Make sure that the 2 pis are of the same length.
+
+    >>> combine("0000", "1111")
+    >>> combine("0001", "1001")
+    '-001'
+    >>> combine('10--', '00--')
+    '-0--'
+    >>> combine('1--1', '1--0')
+    '1---'
+    """
+    
+    diff_list = [] # Keeps track of whick indices in the bit string are different
+
+    for i in range(len(pi1)):
+        if pi1[i] != pi2[i]:
+            diff_list.append(i)
+
+    if len(diff_list) == 1:
+        # The PIs can be combined
+        return pi1[:diff_list[0]] + '-' + pi1[diff_list[0] + 1:]
+
+    else:
+        # The PIs can't be combined if more than 1 bit are different
+        return None
+
+    # 0 bit difference == identical PIs (can never happen in the first place)
 
 
+def gen_epi(minterms, sim_pis, comb_register):
+    """
+    Given a list of prime implicants that can't be simplified further, finds and
+    returns a list of essential PIs. Based on the step 2 (prime implication chart)
+    of Quine-McCluskey algorithm. 
+
+    comb_register stores which minterms were combined to eventually form the 
+    sim_pis.
+
+    Test copied from Quine-McCluskey algorithm's Wikipedia page:
+
+    >>> minterms = [4,8,9,10,11,12,14,15]
+
+    >>> sim_pis = ["-100", "10--", "1--0", "1-1-"]
+
+    >>> comb_register = {"-100" : [4,12], "10--":[8,9,10,11], "1--0" :[8,10,12,14], "1-1-":[10, 11, 14, 15]}
+
+    >>> gen_epi(minterms, sim_pis, comb_register)
+    ['-100', '10--', '1-1-']
+
+    """
+    EPI = []
+
+    # Finding minterms that are formed by just 1 sim_pi
+    pi_tally = {i:[] for i in minterms}
+
+    # Keeps track of which minterms are already covered by the contents of EPI
+    minterms_covered = []
+
+    for pi in sim_pis:
+        # Loop over all the simplified PIs
+        for minterm in comb_register[pi]:
+            # Run over all the minterms that were combined to get this PI
+            pi_tally[minterm].append(pi)
+            # Add this PI to that minterm's PI-tally
+
+    for minterm in pi_tally:
+        if len(pi_tally[minterm]) == 1:
+            # This minterm can be formed only by 1 PI
+            # This PI is EPI
+            EPI.append(pi_tally[minterm][0])
+
+            # Pulling out all the minterms that can be covered by this EPI
+            # Putting them in the list of minterms covered
+            minterms_covered += comb_register[pi_tally[minterm][0]]
+
+    # Work left
+    minterms_left = [i for i in minterms if i not in minterms_covered]
+
+    while minterms_left:
+        
+        minterm = minterms_left.pop()
+
+        possible_choices = pi_tally[minterm]
+        choice = max(possible_choices, key = lambda x: len(comb_register[x]))
+
+        EPI.append(choice)
+        # The choice PI will have some other minterms that it covers, which might
+        # be there in the list of minterms_left. Pop them off
+
+        for i in comb_register[choice]:
+            if i in minterms_left: minterms_left.remove(i)
+
+
+    return EPI
+
+def form_function(epis, vars):
+    """
+    Given a list of epis and the desired vars (same order), returns a 
+    string that can be used for forming a BF.
+    
+    This function assumes that epis actually contain epis! It does not do any
+    simplification. Can produce unexpected results if called that way.
+
+    This will also simiplify using the identity:
+    a + a = a
+
+    The similar simiplification using a*a = a is not done! Other parts of who 
+    call this should do it!
+
+    >>> form_function(['001', '-1-'], ['a', 'b', 'c'])
+    '~a*~b*c + b'
+
+    >>> form_function(['---'], ['a','b','c'])
+    '1'
+    """
+
+    # This removes repetitive EPIs which might have lingered on.
+    epis = list(set(epis))
+
+    epis.sort(key = find_ones_pi)
+    rv = ""
+    for epi in epis:
+        # Going over all the EPIs
+
+        term = ""
+        for i in range(len(vars)):
+            # Parsing this EPI bit by bit
+                if epi[i] == "0":
+                    # Term needs a negated form of this variable
+                    if term != "":
+                        term += "*~" + vars[i]
+                    else:
+                        term += "~" + vars[i]
+
+                elif epi[i] == "1":
+                    # Term needs a negated form of this variable
+                    if term != "":
+                        term += "*" + vars[i]
+                    else:
+                        term += vars[i]
+
+                # Else, this bit can be "-", meaning variable is not needed. Skip
+
+        if rv != "":
+            rv += " + " + term
+        else:
+            rv += term
+
+    if rv == "":
+        # All the variables have a "-". It is the always true function
+        return "1"
+    else:
+        return rv
 
 def test_vals(minterms):
     """
@@ -277,6 +578,11 @@ def test_vals(minterms):
 def find_ones(n):
     """
     Given a number n, returns the number of 1's in its binary representation.
+
+    >>> find_ones(10)
+    2
+    >>> find_ones(7)
+    3
     """
     binary = bin(n)[2:]
     return sum([int(i) for i in binary])
@@ -285,6 +591,11 @@ def find_zeros(n, bits):
     """
     Given a number n, returns the number of 0's in its binary representation.
     Considers the "bits" number of bits
+    
+    >>> find_zeros(7, 4)
+    1
+    >>> find_zeros(7, 3)
+    0
     """
     return (bits - find_ones(n))
 
@@ -294,9 +605,27 @@ def bin_conv(n, bits):
 
     If "bits", can't completely contain all the bits, returns the lowest sig 
     bits.
+
+    >>> bin_conv(10, 5)
+    '01010'
+    >>> bin_conv(7, 2)
+    '11'
+    >>> bin_conv(7,3)
+    '111'
     """
     rv = bin(n)[2:]
     if len(rv) >= bits: return rv[:bits]
 
     else:
         return "0"*(bits - len(rv)) + rv
+
+def find_ones_pi(pi):
+    """
+    Given a PI (in its standard format), returns the number of 1's contained in 
+    it.
+    """
+    rv = 0
+    for i in pi:
+        if i == "1": rv += 1
+
+    return rv
--- strings.py
+++ strings.py
@@ -209,7 +209,113 @@ def special_replace(s, c, replacor):
 
     return s
     
+
+def proper(function, name = "f"):
+    """
+    Given a Boolean function (just expression without f(...) notation), converts
+    it to standard notation that parse can understand. Can be used as a helper
+    with parse.
+
+    This has the caability of handline only 2 cases, both of which are logically 
+    correct, but just use different semantics.
+
+    e.g. "a + b*c" == "f(a,b,c) = a + b*c"
+    It returns a standard string "f(a, b, c) = a + b*c" in this case.
     
+    By default, the function generated is f.
+
+    >>> proper("a+b*c")
+    'f(a, b, c) = a+b*c'
+    
+    >>> proper("f(a,b,c) = a+b*c")
+    'f(a,b,c) = a+b*c'
+
+    >>> proper('f = a+b+c')
+    'f(a, b, c) = a+b+c'
+
+    >>> proper('f(a,b,c = a+b+c')
+    'fabc(a, b, c) = a+b+c'
+    
+    >>> proper('fa,b,c) = a+b+c')
+    'fabc(a, b, c) = a+b+c'
+
+    >>> proper('f   a,b,c) = a+b+c')
+    'fabc(a, b, c) = a+b+c'
+
+    >>> proper('f  a,b,c)=a+b+c')
+    'fabc(a, b, c) = a+b+c'
+    """
+    variables = set() # All the variables found in the expression
+    trash = list() # Everything else that is found in the expression
+
+    # Just for backing up the order. There is a possibility that the given
+    # function was partially proper.
+    possible_name = "" 
+
+
+    index = 0
+    flag = 0 # Flag for the case where just function name was given without vars
+
+    for c in function:
+        if c == "=":
+            if ")" in trash and "(" in trash:
+                # Proper notation was passed in. Ignore the variables set and return
+                # the function itself
+                return function 
+            else:
+                # Function name was given without listing variables
+                # Partially proper syntax. Set the flag
+                flag = 1
+                break
+        else: 
+            if 97 <= ord(c) <= 122 or 65 <= ord(c) <= 90:
+                # c is an english letter, so a variable, or part of name (partially proper case)
+                variables.add(c)
+                possible_name += c
+
+            else:
+                trash.append(c)
+        index += 1
+
+    if flag == 1:
+        # Chop off the function name, and pass it separately in a recursive call
+        if function[index+1] == ' ':
+            # If spacing was used after "="
+            return proper(function[index+2:], possible_name)
+        else:
+            # If spacing wasn't used after "="
+            return proper(function[index+1:], possible_name)
+    
+    else:
+        # Sort the variables alphabetically
+
+        variables = list(variables)
+        variables.sort()
+
+        var_string = ""
+        for i in variables:
+            var_string += i + ", "
+        var_string = var_string[:-2]         
+
+        return ("%s(%s) = %s" %(name, var_string, function))
+
+def find_name(function):
+    """
+    Given a function string in the proper format, traces out the name of the 
+    function.
+
+    >>> find_name("f(a,b,c) = a+b+c")
+    'f'
+
+    >>> find_name("apple(a,e,g) = ~a")
+    'apple'
+    """
+    rv = ""
+    for c in function:
+        if c != "(": 
+            rv += c
+        else:
+            return rv
     
     
     
